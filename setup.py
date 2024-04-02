29b3990aad8ecc5f22e8bceb6723a352151c32c1
Abhishek-Khanduri <khanduriabhishek012@gmail.com>
2024-04-02 13:33:11 +0530

    cleanups

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open('requirements.txt') as f:
 
 
 setup(
-    name='django-ok-likes',
-    version='0.7.4',
-    description='Django likes app',
+    name='django-ok-likes-latest',
+    version='0.8.0',
+    description='Django likes app latest',
     long_description=open('README.rst').read(),
-    author='Oleg Kleschunov',
-    author_email='igorkleschunov@gmail.com',
-    url='https://github.com/LowerDeez/ok-likes',
+    author='Abhishek Khanduri',
+    author_email='khanduriabhishek012@gmail.com',
+    url='https://github.com/ColoredCow/ok-likes',
     packages=[pkj_name] + [pkj_name + '.' + x for x in find_packages(pkj_name)],
     include_package_data=True,
     license='MIT',
@@ -22,11 +22,9 @@ setup(
         'Environment :: Web Environment',
         "Development Status :: 4 - Beta",
         "Operating System :: OS Independent",
-        "Framework :: Django :: 2.0",
-        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 5.0",
         'Intended Audience :: Developers',
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.10.13",
     ]
 
 )
